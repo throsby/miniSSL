8a00d6f7a1fc8840d792d9bf2517c739f5572678
Jack <z3484290@zmail.unsw.edu.au>
2016-06-01 19:14:15 +1000

    Reformated

--- client.py
+++ client.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 # To run this in terminal: python client.py "127.0.0.1" 50000 "certs/minissl-client.pem" "certs/minissl-client.key.pem"
 
 from Crypto.Cipher import PKCS1_OAEP
@@ -14,18 +16,21 @@ import struct
 import Padding
 
 # DEFINES
+
 RECV_HEAD = 0
 RECV_NONCE = 1
 RECV_CERT = 3
-CERT_REQ  = 4
+CERT_REQ = 4
 
-path_to_ca_cert = "certs/minissl-ca.pem"
+path_to_ca_cert = 'certs/minissl-ca.pem'
 
 # Run arguments
+
 DESTINATION_IP = sys.argv[1]
 DESTINATION_PORT = sys.argv[2]
 CLIENT_CERT = sys.argv[3]
 CLIENT_PRIVATEKEY = sys.argv[4]
+
 
 ###################
 
@@ -35,9 +40,11 @@ def verifyHash(key, all_msgs, recv_hash):
         return 1
     return 0
 
+
 def smartSend(socket, data):
     data = struct.pack('>I', len(data)) + data
     socket.sendall(data)
+
 
 def smartRecv(sock):
     message_length = recvHelper(sock, 4)
@@ -46,6 +53,7 @@ def smartRecv(sock):
     message_length = struct.unpack('>I', message_length)[0]
 
     return recvHelper(sock, message_length)
+
 
 def recvHelper(sock, message_length):
     data = ''
@@ -56,19 +64,25 @@ def recvHelper(sock, message_length):
         data += packet
     return data
 
+
 # THIS HELPER VALIDATES CERTIFICATE #
+
 def validate_certificate(recv_certificate):
-    if not keyutils.verify_certificate(readCertificate(path_to_ca_cert), recv_certificate):
-        print "Bad Certificate"
+    if not keyutils.verify_certificate(readCertificate(path_to_ca_cert),
+            recv_certificate):
+        print 'Bad Certificate'
         return 0
-    elif keyutils.read_issuer(recv_certificate) != keyutils.read_issuer(readCertificate("certs/minissl-ca.pem")):
-            print "Bad Issuer"
-            return 0
+    elif keyutils.read_issuer(recv_certificate) \
+        != keyutils.read_issuer(readCertificate('certs/minissl-ca.pem'
+                                )):
+        print 'Bad Issuer'
+        return 0
     else:
         return 1
 
 
 # THIS HELPER READS CERTIFICATE FROM FILE #
+
 def readCertificate(file_path):
     f = open(file_path)
     cert = f.read()
@@ -77,12 +91,13 @@ def readCertificate(file_path):
 
 
 # THIS HELPER INITIALISES THE CONNECTIONS #
+
 def initialise(socket):
-    all_recv_msgs = ""
-    all_sent_msgs = ""
-    print "Connected. Initiating handshake..."
+    all_recv_msgs = ''
+    all_sent_msgs = ''
+    print 'Connected. Initiating handshake...'
     initNonce = keyutils.generate_nonce(28)
-    initMsg = ("ClientInit", initNonce, "AES-HMAC")
+    initMsg = ('ClientInit', initNonce, 'AES-HMAC')
     initMsg = pickle.dumps(initMsg)
     all_sent_msgs += initMsg
     smartSend(socket, initMsg)
@@ -91,42 +106,54 @@ def initialise(socket):
     all_recv_msgs += data
 
     # VALIDATING CERTIFICATE (See helper function above) #
+
     if not validate_certificate(initResponse[RECV_CERT]):
-        print "Bad Certificate!"
+        print 'Bad Certificate!'
         return
 
     # DERIVING PUBLIC KEY FROM CERTIFICATE + PRIVATE KEY#
+
     public_key = keyutils.read_pubkey_from_pem(initResponse[RECV_CERT])
-    private_key = keyutils.read_privkey_from_pem(readCertificate(CLIENT_PRIVATEKEY))
+    private_key = \
+        keyutils.read_privkey_from_pem(readCertificate(CLIENT_PRIVATEKEY))
 
     # COMPUTING SECRET #
+
     secret = keyutils.generate_random(46)
 
     # DERIVING KEYS FROM SECRET #
-    session_key_one = keyutils.create_hmac(secret, initResponse[RECV_NONCE] + initNonce + '00000000')
-    session_key_two = keyutils.create_hmac(secret, initResponse[RECV_NONCE] + initNonce + '11111111')
+
+    session_key_one = keyutils.create_hmac(secret,
+            initResponse[RECV_NONCE] + initNonce + '00000000')
+    session_key_two = keyutils.create_hmac(secret,
+            initResponse[RECV_NONCE] + initNonce + '11111111')
 
     msgHMAC = keyutils.create_hmac(session_key_two, all_recv_msgs)
     encrypted = keyutils.encrypt_with_rsa_hybrid(secret, public_key)
 
-    confirm_msg = ("ClientKex:", encrypted[0], encrypted[1], encrypted[2], msgHMAC,)
+    confirm_msg = ('ClientKex:', encrypted[0], encrypted[1],
+                   encrypted[2], msgHMAC)
 
     # IF CERTIFICATE REQUIRED THEN SEND IT
+
     if len(initResponse) == 5:
-        if initResponse[CERT_REQ] == "CertReq":
-            signedNonse = private_key.sign(initResponse[RECV_NONCE], private_key)
-            confirm_msg = confirm_msg + (readCertificate("certs/minissl-client.pem"), str(signedNonse[0]), )
+        if initResponse[CERT_REQ] == 'CertReq':
+            signedNonse = private_key.sign(initResponse[RECV_NONCE],
+                    private_key)
+            confirm_msg = confirm_msg \
+                + (readCertificate('certs/minissl-client.pem'),
+                   str(signedNonse[0]))
     p = pickle.dumps(confirm_msg)
 
     all_sent_msgs += p
     smartSend(socket, p)
     finalMsg = smartRecv(socket)
     if not verifyHash(session_key_two, all_recv_msgs, finalMsg):
-        print "BAD HASH!"
+        print 'BAD HASH!'
         return
 
-    print "Handshake was succesful. Sending GET command..."
-    get_message = ("GET",)
+    print 'Handshake was succesful. Sending GET command...'
+    get_message = ('GET', )
     pickle_message = pickle.dumps(get_message)
     smartSend(socket, pickle_message)
     file_data_pickle = smartRecv(socket)
@@ -135,17 +162,17 @@ def initialise(socket):
     aes_cipher = AES.new(session_key_one, AES.MODE_CFB, file_data[0])
     file_data = aes_cipher.decrypt(file_data[1])
     file_data = Padding.removePadding(file_data)
-    f = open("received_payload.txt", 'wb')
+    f = open('received_payload.txt', 'wb')
     f.write(file_data)
     f.close()
-    print "File received."
+    print 'File received.'
     socket.close()
-    print "Client terminated."
+    print 'Session terminated.'
     sys.exit()
 
 
 SERVER_IP = DESTINATION_IP
-print "Attempting to connect to: " + SERVER_IP
+print 'Attempting to connect to: ' + SERVER_IP
 receiveSem = threading.Semaphore([1])
 socket = socket(AF_INET, SOCK_STREAM)
 socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 # To run this in terminal: python server.py 50000 "certs/minissl-server.pem" "certs/minissl-server.key.pem" "ClientAuth/SimpleAuth" "payload.txt"
 
 from Crypto.Cipher import PKCS1_OAEP
@@ -6,7 +8,7 @@ from Crypto.Cipher import AES
 from socket import *
 import threading
 import ast
-from binascii import hexlify,unhexlify
+from binascii import hexlify, unhexlify
 import sys
 from base64 import b64decode
 import keyutils
@@ -20,35 +22,52 @@ import Padding
 # 3: Encrypted AES key
 # 4: msgHMAC
 # 5: OPTIONAL Client certificate
+
 RECV_CERT = 5
 
 # Run arguments
+
 LISTEN_PORT = sys.argv[1]
 SERVERCERT = sys.argv[2]
 SERVERPRIVKEY = sys.argv[3]
 AUTHMETHOD = sys.argv[4]
 PAYLOAD = sys.argv[5]
 
+
 class Client(threading.Thread):
-    def __init__(self, server_socket, client_socket, address):
+
+    def __init__(
+        self,
+        server_socket,
+        client_socket,
+        address,
+        ):
+
         # type: (socket, address) -> client
+
         threading.Thread.__init__(self)
         self.server_sock = server_socket
         self.client_sock = client_socket
         self.addr = address
         self.start()
 
-    def verifyHash(self, key, all_msgs, recv_hash):
+    def verifyHash(
+        self,
+        key,
+        all_msgs,
+        recv_hash,
+        ):
         hashed_msgs = keyutils.create_hmac(key, all_msgs)
         if hashed_msgs == recv_hash:
             return 1
         return 0
 
-
     # THIS HELPER VALIDATES CERTIFICATE #
-    def validate_certificate(self,recv_certificate):
-        if not keyutils.verify_certificate(self.readCertificate("certs/minissl-ca.pem"), recv_certificate):
-            print "Bad Certificate!"
+
+    def validate_certificate(self, recv_certificate):
+        if not keyutils.verify_certificate(self.readCertificate('certs/minissl-ca.pem'
+                ), recv_certificate):
+            print 'Bad Certificate!'
             return 0
         else:
             return 1
@@ -81,20 +100,21 @@ class Client(threading.Thread):
 
     @staticmethod
     def init_connection(self, message_tuple):
-        all_sent_msgs = ""
-        all_recv_msgs = ""
+        all_sent_msgs = ''
+        all_recv_msgs = ''
 
         cert = self.readCertificate(SERVERCERT)
         clientNonce = message_tuple[1]
         initNonce = keyutils.generate_nonce(28)
 
         if AUTHMETHOD == 'ClientAuth':
-            reqClientCert = "CertReq"
-        else :
-            reqClientCert = ""
+            reqClientCert = 'CertReq'
+        else:
+            reqClientCert = ''
 
-        print "Initiating handshake..."
-        initMsg = ("ServerInit", initNonce, message_tuple[2], cert, reqClientCert,)
+        print 'Initiating handshake...'
+        initMsg = ('ServerInit', initNonce, message_tuple[2], cert,
+                   reqClientCert)
         initMsg = pickle.dumps(initMsg)
         all_sent_msgs += initMsg
         self.smartSend(self.client_sock, initMsg)
@@ -104,54 +124,64 @@ class Client(threading.Thread):
         initResponse = pickle.loads(data)
 
         # VALIDATING CERTIFICATE #
+
         if reqClientCert:
             if not self.validate_certificate(initResponse[RECV_CERT]):
-                print "Bad Certificate!"
+                print 'Bad Certificate!'
                 return
 
         # DERIVING PUBLIC KEY FROM CERTIFICATE + PRIVATE KEY#
-        private_key = keyutils.read_privkey_from_pem(self.readCertificate(SERVERPRIVKEY))
+
+        private_key = \
+            keyutils.read_privkey_from_pem(self.readCertificate(SERVERPRIVKEY))
 
         # COMPUTING SECRET #
+
         rsa_cipher = PKCS1_OAEP.new(private_key)
         aes_key = rsa_cipher.decrypt(initResponse[3])
         aes_cipher = AES.new(aes_key, AES.MODE_CFB, initResponse[2])
         secret = aes_cipher.decrypt(initResponse[1])
 
         # DERIVING KEYS FROM SECRET #
-        session_key_one = keyutils.create_hmac(secret, initNonce + clientNonce + '00000000')
-        session_key_two = keyutils.create_hmac(secret,  initNonce + clientNonce + '11111111')
 
-        if not self.verifyHash(session_key_two, all_sent_msgs, initResponse[RECV_CERT - 1]):
-            print "BAD HASH"
+        session_key_one = keyutils.create_hmac(secret, initNonce
+                + clientNonce + '00000000')
+        session_key_two = keyutils.create_hmac(secret, initNonce
+                + clientNonce + '11111111')
+
+        if not self.verifyHash(session_key_two, all_sent_msgs,
+                               initResponse[RECV_CERT - 1]):
+            print 'BAD HASH'
             return
         finalMsg = keyutils.create_hmac(session_key_two, all_sent_msgs)
         all_sent_msgs += finalMsg
 
         self.smartSend(self.client_sock, finalMsg)
 
-        print "Handshake was succesful. Waiting for command..."
+        print 'Handshake was succesful. Waiting for command...'
         data = self.smartRecv(self.client_sock)
         command = pickle.loads(data)
 
-        if (command[0] == "GET"):
+        if command[0] == 'GET':
+
             # FINAL STEP #
-            print "Received GET command. Sending file..."
+
+            print 'Received GET command. Sending file...'
             file = open(PAYLOAD, 'r')
             file_data = file.read()
             init_vector = keyutils.generate_random(16)
-            aes_cipher = AES.new(session_key_one, AES.MODE_CFB, init_vector)
+            aes_cipher = AES.new(session_key_one, AES.MODE_CFB,
+                                 init_vector)
             file_data = Padding.appendPadding(file_data)
             encrypted_data = aes_cipher.encrypt(file_data)
             pickle_payload = (init_vector, encrypted_data)
             pickle_payload = pickle.dumps(pickle_payload)
             self.smartSend(self.client_sock, pickle_payload)
-            print "File sent to client."
-
+            print 'File sent to client.'
 
     def run(self):
-        print "Connected to:"
-        print(address)
+        print 'Connected to:'
+        print address
         while 1:
             message = self.smartRecv(self.client_sock)
             if not message:
@@ -159,23 +189,24 @@ class Client(threading.Thread):
 
             message_tuple = pickle.loads(message)
 
-            if (message_tuple[0] == "ClientInit"):
+            if message_tuple[0] == 'ClientInit':
                 try:
                     self.init_connection(self, message_tuple)
                 except Exception, e:
-                    print "Transfer failed with error: " + e
+                    print 'Transfer failed with error: ' + e
                 else:
-                    print "Transfer was succesful."
+                    print 'Transfer was succesful.'
                 finally:
                     self.client_sock.close()
-                    print "Session terminated."
+                    print 'Session terminated.'
                     break
 
+
 server_socket = socket(AF_INET, SOCK_STREAM)
-server_socket.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
+server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 server_socket.bind(('', int(LISTEN_PORT)))
 server_socket.listen(5)
 
 while 1:
-     (client_socket, address) = server_socket.accept()
-     Client(server_socket, client_socket, address)
+    (client_socket, address) = server_socket.accept()
+    Client(server_socket, client_socket, address)
